41e383b1e25fc1a25a7bf8afa783bc53a30d8b8b
Illmaticno1 <demetrius.robinson@icloud.com>
2017-11-10 16:05:43 -0500

    paddles are moving

---
+++ app.js
@@ -0,0 +1,44 @@
+//create function to move the players divs
+console.log($('#paddle_1'));
+
+
+$( () => {
+
+
+  $(document).keydown( (e) => {
+    // console.log(e.which);
+    switch (e.which) {
+      case 37:
+      $('#paddle_1').animate(
+        {
+          left: '-=5'
+
+        });
+        break;
+        case 39:
+        $('#paddle_1').animate(
+          {
+            left: '+=5'
+          });
+          break;
+          case 65:
+          $('#paddle_2').animate(
+            {
+              left: '-=5'
+            });
+            break;
+            case 83:
+            $('#paddle_2').animate(
+              {
+                left: '+=5'
+              });
+              break;
+              default:
+
+
+
+        }
+      });
+
+
+    });
