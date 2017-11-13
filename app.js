a1acccedc63625b91cd5a21fefe79797338094a7
Illmaticno1 <demetrius.robinson@icloud.com>
2017-11-13 17:36:23 -0500

    player divs have a better feel

--- app.js
+++ app.js
@@ -1,90 +1,112 @@
 //create function to move the players divs
-console.log($('#paddle_1'));
 
 
-$( () => {
+$(document).ready( () => {
+
+  let paddleOne = $('#paddle_1');
+  let paddleTwo = $('#paddle_2');
+  let ball = $('#ball');
+
+  let top = 6;
+  let moveBall = true;
+
+  
+
+console.log(paddleOne.css('left'));
+  //  global functions used for call backs
+
+  let left_p1 = () => {
+    if (parseInt(paddleOne.css('left')) > 0) {
+      $('#paddle_1').animate(
+      {left: '-=15'},
+      {duration: 1});
+    };
+  };
 
 
-  //  let left_p1 = () => {
-  //    $('#paddle_1').event.currentTarget().css('left', '-=5');
-  //  }
+  let right_p1 = () => {
+    if (parseInt(paddleOne.css('left')) < 310) {
+      $('#paddle_1').animate(
+     {left: '+=15'},
+     {duration: 1});
+    };
+  };
 
 
-  $(document).keyup( (e) => {
+  let left_p2 = () => {
+    if (parseInt(paddleTwo.css('left')) > 0) {
+      $('#paddle_2').animate(
+      {left: '-=15'},
+      {duration: 1});
+    };
+  };
+
+
+  let right_p2 = () => {
+    if (parseInt(paddleTwo.css('left')) < 310) {
+      $('#paddle_2').animate(
+     {left: '+=15'},
+     {duration: 1});
+    };
+  };
+
+  let stop = () => {
+    $(('#paddle').css('left') +=0);
+  }
+
+
+
+
+
+  // key up and key down function to move player divs
+  $(document).keydown( (e) => {
     // console.log(e.which);
     switch (e.which) {
       case 37:
-      $('#paddle_1').animate(
-        {
-          left: '-=0'
+      left_p1();
+      break;
 
-        });
-        break;
-        case 39:
-        $('#paddle_1').animate(
-          {
-            left: '+=0'
-          });
-          break;
-          case 65:
-          $('#paddle_2').animate(
-            {
-              left: '-=0'
-            });
-            break;
-            case 83:
-            $('#paddle_2').animate(
-              {
-                left: '+=0'
-              });
-              break;
-              default:
-              break;
-            }
-          });
+      case 39:
+      right_p1();
 
+      break;
 
-          $(document).keydown( (e) => {
-            let playerOneLeft = false;
-            // console.log(e.which);
-            switch (e.which) {
-              case 37:
+      case 65:
+      left_p2();
 
-              // if ($('#paddle_1').css('left') > 0% {
-                $('#paddle_1').animate(
-                  
-                  {
-                    left: '-=20'
-                  })
-                // })
+      break;
+      case 83:
+      right_p2();
 
-                break;
-                case 39:
-                $('#paddle_1').animate(
-                  {
-                    left: '+=20'
-                    // playerOneRight = requestAnimationFrame(right_p1);
-                  });
-                  break;
-                  case 65:
-                  $('#paddle_2').animate(
-                    {
-                      left: '-=20'
-                      // playerTwoLeft = requestAnimationFrame(left_p2);
-                    });
-                    break;
-                    case 83:
-                    $('#paddle_2').animate(
-                      {
-                        left: '+=20'
-                        // playerTwoRight = requestAnimationFrame(right_p2);
-                      });
-                      break;
-                      default:
-                      break;
-                    }
-                  });
+    break;
+    default:
+    break;
+    }
+    });
 
 
 
-                });
+    let play = () => {
+
+      if(moveBall === true){
+        down();
+      } else {
+        up();
+      }
+      setInterval(play);
+    };
+
+    let down = () => {
+      // console.log(ball);
+      ball.css('top', parseInt(ball.css('top')) + top);
+    }
+
+    // let up = () => {
+    //   ball.css('top', parseInt(ball.css('top')) - top);
+    // }
+    // play();
+
+
+
+
+});
