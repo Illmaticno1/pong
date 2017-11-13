126f622a3354d7f64f9bd5c3dafc969bafe5e502
Illmaticno1 <demetrius.robinson@icloud.com>
2017-11-12 23:45:33 -0500

    still working on animations

--- app.js
+++ app.js
@@ -11,77 +11,80 @@ $( () => {
 
 
   $(document).keyup( (e) => {
-// console.log(e.which);
-switch (e.which) {
-case 37:
-$('#paddle_1').animate(
-{
-  left: '-=0'
-
-});
-break;
-case 39:
-$('#paddle_1').animate(
-  {
-    left: '+=0'
-  });
-  break;
-  case 65:
-  $('#paddle_2').animate(
-    {
-      left: '-=0'
-    });
-    break;
-    case 83:
-    $('#paddle_2').animate(
-      {
-        left: '+=0'
-      });
-      break;
-      default:
-      break;
-    }
-  });
-
-
-  $(document).keydown( (e) => {
-    let playerOneLeft = false;
-  // console.log(e.which);
-  switch (e.which) {
-    case 37:
-    $('#paddle_1').animate(
-      {
-        left: '-=5'
-        // playerOneLeft = $('#paddle_1').requestAnimationFrame();
-
-      });
-      break;
-      case 39:
+    // console.log(e.which);
+    switch (e.which) {
+      case 37:
       $('#paddle_1').animate(
         {
-          left: '+=5'
-          // playerOneRight = requestAnimationFrame(right_p1);
+          left: '-=0'
+
         });
         break;
-        case 65:
-        $('#paddle_2').animate(
+        case 39:
+        $('#paddle_1').animate(
           {
-            left: '-=5'
-            // playerTwoLeft = requestAnimationFrame(left_p2);
+            left: '+=0'
           });
           break;
-          case 83:
+          case 65:
           $('#paddle_2').animate(
             {
-              left: '+=5'
-              // playerTwoRight = requestAnimationFrame(right_p2);
+              left: '-=0'
             });
             break;
-            default:
-            break;
-          }
-        });
+            case 83:
+            $('#paddle_2').animate(
+              {
+                left: '+=0'
+              });
+              break;
+              default:
+              break;
+            }
+          });
+
+
+          $(document).keydown( (e) => {
+            let playerOneLeft = false;
+            // console.log(e.which);
+            switch (e.which) {
+              case 37:
+
+              // if ($('#paddle_1').css('left') > 0% {
+                $('#paddle_1').animate(
+                  
+                  {
+                    left: '-=20'
+                  })
+                // })
+
+                break;
+                case 39:
+                $('#paddle_1').animate(
+                  {
+                    left: '+=20'
+                    // playerOneRight = requestAnimationFrame(right_p1);
+                  });
+                  break;
+                  case 65:
+                  $('#paddle_2').animate(
+                    {
+                      left: '-=20'
+                      // playerTwoLeft = requestAnimationFrame(left_p2);
+                    });
+                    break;
+                    case 83:
+                    $('#paddle_2').animate(
+                      {
+                        left: '+=20'
+                        // playerTwoRight = requestAnimationFrame(right_p2);
+                      });
+                      break;
+                      default:
+                      break;
+                    }
+                  });
 
 
 
-        });
+                });
