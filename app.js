dbec1229f074f13fc49d0bc30417272108289b31
Illmaticno1 <demetrius.robinson@icloud.com>
2017-11-13 21:53:49 -0500

    we have some sort of collision detection

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 
 $(document).ready( () => {
 
+// global variables
   let pw, ph, px, py, tw, th, tx, ty;
   let paddleOne = $('#paddle_1');
   let paddleTwo = $('#paddle_2');
@@ -14,65 +15,53 @@ $(document).ready( () => {
   let p = ball;
   let t = paddleOne;
 
-
-  //  global functions used for call backs
-  let detectCollisons = () => {
-
-      pw = p.offsetWidth;
-      ph = p.offsetHeight;
-      px = p.offsetLeft;
-      py = p.offsetTop;
-      tw = t.offsetWidth;
-      th = t.offsetHeight;
-      tx = t.offsetLeft;
-      ty = t.offsetTop;
-
-      if((px+pw) > tx && px < (tx+tw) && (py+ph) > ty && py < (ty+th)){
-        console.log('collision detected');
-      }
-      window.requestAnimationFrame(detectCollisons);
-  }
+  console.log(t.position().left);
+  console.log(t.width());
+  console.log(p.position().left + t.width());
 
 
 
+
+
+  // move paddle_1 left
   let left_p1 = () => {
     if (parseInt(paddleOne.css('left')) > 0) {
       $('#paddle_1').animate(
-      {left: '-=15'},
-      {duration: 1});
+        {left: '-=15'},
+        {duration: 1});
+      };
     };
-  };
 
-
+  // move paddle_1 right
   let right_p1 = () => {
     if (parseInt(paddleOne.css('left')) < 310) {
       $('#paddle_1').animate(
-     {left: '+=15'},
-     {duration: 1});
+        {left: '+=15'},
+        {duration: 1});
+      };
     };
-  };
 
-
+  // move paddle_2 left
   let left_p2 = () => {
     if (parseInt(paddleTwo.css('left')) > 0) {
       $('#paddle_2').animate(
-      {left: '-=15'},
-      {duration: 1});
+        {left: '-=15'},
+        {duration: 1});
+      };
     };
-  };
 
-
+  // move paddle_2 right
   let right_p2 = () => {
     if (parseInt(paddleTwo.css('left')) < 310) {
       $('#paddle_2').animate(
-     {left: '+=15'},
-     {duration: 1});
+        {left: '+=15'},
+        {duration: 1});
+      };
     };
-  };
 
   let stop = () => {
-    $(('#paddle').css('left') +=0);
-  }
+      $(('#paddle').css('left') +=0);
+    }
 
 
 
@@ -98,21 +87,21 @@ $(document).ready( () => {
       case 83:
       right_p2();
 
-    break;
-    default:
-    break;
-    }
+      break;
+      default:
+      break;
+      }
     });
 
 
-
+    // game start function
     let play = () => {
-
       if(detectCollisons(t, p))
       {
         up();
         console.log('yes');
       }
+
       if(moveBall === true)
       {
         down();
@@ -121,18 +110,41 @@ $(document).ready( () => {
       {
         up();
       }
-      setInterval(play), 1000;
+      setInterval(play, 2000);
     };
 
 
-
+    // ball movement functions
     let down = () => {
       // console.log(ball);
-      ball.css('top', parseInt(ball.css('top')) + top);
+      ball.css('top', parseInt(ball.css('top')) + 40);
     }
+
     let up = () => {
       ball.css('top', parseInt(ball.css('top')) - top);
     }
+
+    let detectCollisons = () => {
+      // console.log(t.position().top);
+      console.log(p.position().top);
+      if((t.position().top < p.position().top)
+       &&
+       (t.position().left < p.position().left)
+        &&
+        (t.position().left + t.width() > p.position().left + p.width())){
+        console.log('contact');
+
+        up();
+      }
+    }
+    // let changeDirection = () => {
+    //   if (down === true) {
+    //     up();
+    //   } else if (up === true){
+    //     down();
+    //   }
+    // }
+
     play();
 
 
