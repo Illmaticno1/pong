bcd65f589641137bf842e5071b5c8cd8e2dc87af
Illmaticno1 <demetrius.robinson@icloud.com>
2017-11-14 11:54:25 -0500

    fixing collision detection for player two

--- app.js
+++ app.js
@@ -3,18 +3,20 @@
 
 $(document).ready( () => {
 
-// global variables
-  let pw, ph, px, py, tw, th, tx, ty;
+  // global variables
+  // let pw, ph, px, py, tw, th, tx, ty; unused variables
   let paddleOne = $('#paddle_1');
   let paddleTwo = $('#paddle_2');
   let ball = $('#ball');
   let downward = true;
   let upward = false;
   let top = 6;
-
+  let container = $('#container');
 
   let p = ball;
   let t = paddleOne;
+  let t2 = paddleTwo;
+  let height = paddleTwo.height();
 
   console.log(t.position().left);
   console.log(t.width());
@@ -33,130 +35,155 @@ $(document).ready( () => {
       };
     };
 
-  // move paddle_1 right
-  let right_p1 = () => {
-    if (parseInt(paddleOne.css('left')) < 310) {
-      $('#paddle_1').animate(
-        {left: '+=15'},
-        {duration: 1});
+    // move paddle_1 right
+    let right_p1 = () => {
+      if (parseInt(paddleOne.css('left')) < 310) {
+        $('#paddle_1').animate(
+          {left: '+=15'},
+          {duration: 1});
+        };
       };
-    };
 
-  // move paddle_2 left
-  let left_p2 = () => {
-    if (parseInt(paddleTwo.css('left')) > 0) {
-      $('#paddle_2').animate(
-        {left: '-=15'},
-        {duration: 1});
-      };
-    };
+      // move paddle_2 left
+      let left_p2 = () => {
+        if (parseInt(paddleTwo.css('left')) > 0) {
+          $('#paddle_2').animate(
+            {left: '-=15'},
+            {duration: 1});
+          };
+        };
 
-  // move paddle_2 right
-  let right_p2 = () => {
-    if (parseInt(paddleTwo.css('left')) < 310) {
-      $('#paddle_2').animate(
-        {left: '+=15'},
-        {duration: 1});
-      };
-    };
+        // move paddle_2 right
+        let right_p2 = () => {
+          if (parseInt(paddleTwo.css('left')) < 310) {
+            $('#paddle_2').animate(
+              {left: '+=15'},
+              {duration: 1});
+            };
+          };
 
-  let stop = () => {
-      $(('#paddle').css('left') +=0);
-    }
-
+          let stop = () => {
+            $(('#paddle').css('left') +=0);
+          }
 
 
 
 
-  // key up and key down function to move player divs
-  $(document).keydown( (e) => {
-    // console.log(e.which);
-    switch (e.which) {
-      case 37:
-      left_p1();
-      break;
 
-      case 39:
-      right_p1();
+          // key up and key down function to move player divs
+          $(document).keydown( (e) => {
+            // console.log(e.which);
+            switch (e.which) {
+              case 37:
+              left_p1();
+              break;
 
-      break;
+              case 39:
+              right_p1();
 
-      case 65:
-      left_p2();
+              break;
 
-      break;
-      case 83:
-      right_p2();
+              case 65:
+              left_p2();
 
-      break;
-      default:
-      break;
-      }
-    });
+              break;
+              case 83:
+              right_p2();
 
-
-    // game start function
-    let play = () => {
-      detectCollisons();
-      down();
-
-      setInterval(play, 1000);
-    };
-
-
-    // ball movement functions
-    let down = () => {
-      // console.log(ball);
-      ball
-      ball.css('top', parseInt(ball.css('top')) + top);
-      downward = true;
-    }
-
-    let up = () => {
-      ball.css('top', parseInt(ball.css('top')) - top);
-      upward = true;
-    }
-
-    let stopIt = () => {
-      ball.css('top', parseInt(ball.css('top')) - 0);
-      changeDirection();
-    }
-
-
-    // collision detection (works)
-    let detectCollisons = () => {
-      // console.log('paddle:' t.position());
-      // console.log(t.position().top);
-      console.log(p.position());
-      // console.log(p.position().top);
-
-      if(((t.position().top) < p.position().top)
-      &&
-      (t.position().left < p.position().left)
-      &&
-      (t.position().left + t.width() > p.position().left + p.width())){
-        up();
-        // console.log('contact');
-
-        stopIt();
-
-      }
-    }
-
-
-
-    // unfinsihed
-    let changeDirection = () => {
-      if (downward === true) {
-        up();
-      } else if (upward === true){
-        down();
-      }
-    }
-
-    play();
+              break;
+              default:
+              break;
+            }
+          });
 
 
 
 
-});
+
+          // collision detection (works)
+          let detectCollisons1 = () => {
+
+            if(
+              ((t.position().top) < p.position().top + p.height())
+              &&
+              (t.position().left < p.position().left)
+              &&
+              (t.position().left + t.width() > p.position().left + p.width()))
+              {
+                paddleOne.css('background-color', 'green');
+                // console.log('contact');
+                downward = false;
+                console.log(downward);
+                stopIt();
+                console.log(ball.position().top);
+                changeDirection;
+              }
+            }
+
+            let detectCollisons2 = () => {
+              if(
+                (t2.position().top - t2.height > p.position().top)
+                &&
+                (t2.position().left < p.position().left)
+                &&
+                (t2.position().left + t2.width() < p.position().left + p.width())
+              )
+              {
+                paddleTwo.css('background-color', 'green');
+                console.log('contact');
+                downward = false;
+                console.log(downward);
+                stopIt();
+                console.log(ball.position().top);
+                changeDirection;
+              }
+            }
+            console.log("paddle two height: " + t2.height());
+            // create function to increment players scores
+            console.log("container: " + container.position().top);
+            let score = () => {
+              if (ball.position().top < container.position().top)
+              {
+                alert('player 1 scores!!')
+              };
+            }
+
+            // game start function
+            let play = () => {
+              // console.log(t2.position().top + t2.height());
+              detectCollisons1();
+              down();
+              changeDirection();
+              setInterval(play, 250);
+            };
+
+            // unfinsihed
+            let changeDirection = () => {
+              if (downward === false) {
+                up();
+              } else if (downward === true){
+                down();
+              }
+            }
+
+            // // ball movement functions
+            let down = () => {
+              // console.log(ball);
+              ball.css('top', parseInt(ball.css('top')) + top);
+            }
+
+            let up = () => {
+              ball.css('top', parseInt(ball.css('top')) - top);
+              console.log("ball: " + ball.position().top);
+              console.log('paddle 2: ' + paddleTwo.position().top);
+            }
+
+            let stopIt = () => {
+              ball.css('top', parseInt(ball.css('top')) + 0);
+            }
+            play();
+
+
+
+
+          });
